bdcb4950029e1c425119403085cf39514db1e0c9
Emilio <TDTxLE@gmail.com>
2020-10-06 20:10:27 -0600

    enlistar device

--- app/src/main/java/com/example/demo_printer/ConnectPrinter.java
+++ app/src/main/java/com/example/demo_printer/ConnectPrinter.java
@@ -33,11 +33,7 @@ import java.util.Vector;
 
 public class ConnectPrinter extends AppCompatActivity {
 
-    private static final String TAG = "BluetoothConnectMenu";
 
-    // Intent request codes
-    // private static final int REQUEST_CONNECT_DEVICE = 1;
-    private static final int REQUEST_ENABLE_BT = 2;
 
 
     //asd
@@ -45,7 +41,6 @@ public class ConnectPrinter extends AppCompatActivity {
     private BluetoothAdapter bluetoothAdapter;
     private BluetoothPort bluetoothPort;
     private Vector<BluetoothDevice> remoteDevices;
-    private Thread hThread;
 
 
     //componentes
@@ -55,6 +50,46 @@ public class ConnectPrinter extends AppCompatActivity {
     private ListView impesoras;
     private TextView cogigo;
 
+    private void bluetoothSetup()
+    {
+        // Initialize
+        clearBtDevData();
+        bluetoothPort = BluetoothPort.getInstance();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null)
+        {
+            // Device does not support Bluetooth
+            return;
+        }
+        if (!bluetoothAdapter.isEnabled())
+        {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+
+            startActivityForResult(enableBtIntent, 2);
+        }
+    }
+
+
+    private void clearBtDevData()
+    {
+        remoteDevices = new Vector<BluetoothDevice>();
+    }
+
+
+    private void addPairedDevices()
+    {
+        BluetoothDevice pairedDevice;
+        Iterator<BluetoothDevice> iter = (bluetoothAdapter.getBondedDevices()).iterator();
+        while(iter.hasNext())
+        {
+            pairedDevice = iter.next();
+            if(bluetoothPort.isValidAddress(pairedDevice.getAddress()))
+            {
+                remoteDevices.add(pairedDevice);
+                adapter.add(pairedDevice.getName() +"\n["+pairedDevice.getAddress()+"] [Paired]");
+            }
+        }
+    }
 
 
     @Override
@@ -62,8 +97,117 @@ public class ConnectPrinter extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect_printer);
 
-        bluetoothPort = BluetoothPort.getInstance();
+        cogigo = (TextView) findViewById(R.id.device_concect);
+        conectarButton =(Button) findViewById(R.id.decelerate);
+        impesoras = findViewById(R.id.impresoras);
+        imprimirButton = (Button) findViewById(R.id.imprimir);
+
+        bluetoothSetup();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        impesoras.setAdapter(adapter);
+        addPairedDevices();
+
+        impesoras.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3)
+            {
+                BluetoothDevice btDev = remoteDevices.elementAt(arg2);
+                if(bluetoothAdapter.isDiscovering())
+                {
+                    bluetoothAdapter.cancelDiscovery();
+                }
+                cogigo.setText(btDev.getAddress());
+                btConn(btDev);
+            }
+        });
+
+        registerReceiver(discoveryResult, new IntentFilter(BluetoothDevice.ACTION_FOUND));
     }
+
+    public BroadcastReceiver discoveryResult = new BroadcastReceiver()
+    {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            String key;
+            BluetoothDevice remoteDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            if(remoteDevice != null)
+            {
+                if(remoteDevice.getBondState() != BluetoothDevice.BOND_BONDED)
+                {
+                    key = remoteDevice.getName() +"\n["+remoteDevice.getAddress()+"]";
+                }
+                else
+                {
+                    key = remoteDevice.getName() +"\n["+remoteDevice.getAddress()+"] [Pairedd]";
+                }
+                if(bluetoothPort.isValidAddress(remoteDevice.getAddress()))
+                {
+                    remoteDevices.add(remoteDevice);
+                    adapter.add(key);
+                }
+            }
+        }
+    };
+
+
+    private void btConn(final BluetoothDevice btDev){
+
+        if(bluetoothPort.isConnected()){
+            try {
+                bluetoothPort.connect(btDev.getAddress());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            try {
+                bluetoothPort.disconnect();
+                bluetoothPort.connect(btDev.getAddress());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+    public void buscar(View v){
+        if (!bluetoothAdapter.isDiscovering())
+        {
+            clearBtDevData();
+            adapter.clear();
+            bluetoothAdapter.startDiscovery();
+        }
+        else
+        {
+            bluetoothAdapter.cancelDiscovery();
+        }
+    }
+
+   public void imprimir(View v){
+        pirnt();
+    }
+
+    public void desconectar(View v){
+        if(bluetoothPort.isConnected()){
+            try {
+                bluetoothPort.disconnect();
+                cogigo.setText(" ");
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+
+
 
  void pirnt(){
      CPCL cpcl = new CPCL();
